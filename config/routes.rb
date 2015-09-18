ce7b5936542bc75e4b9f5c04254e419f01be53a5
Ingo Stopka <stopka@wtal.de>
2015-09-18 14:36:03 +0000

    v4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  
+
+resources :users
+resources :adresses
+
 end
