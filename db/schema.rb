2cb4d0610b8391f614ccf797d15b961fd7349e84
Ingo Stopka <stopka@wtal.de>
2015-09-26 18:40:37 +0000

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925183745) do
+ActiveRecord::Schema.define(version: 20150926181144) do
 
   create_table "adresses", force: :cascade do |t|
     t.string   "vorname"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150925183745) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+ def setup
+    @user = users(:ingo)
+ end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+  
+end
+
